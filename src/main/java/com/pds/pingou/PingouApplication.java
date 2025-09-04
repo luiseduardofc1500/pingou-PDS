9ca81c56199a7acbb810be524bf96772b0609f38
Luis Candido <103050923+luiseduardofc1500@users.noreply.github.com>
2025-09-04 08:08:48 -0300

    start project

---
+++ src/main/java/com/pds/pingou/PingouApplication.java
@@ -0,0 +1,13 @@
+package com.pds.pingou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PingouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PingouApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pds/pingou/PingouApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pds.pingou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PingouApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
